1fbd2dfd325b4f7c52c2b9d2178dc0be392d4dd5
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-07-09 16:45:49 -0600

    feat: enhance product details screen with data fetching and error handling

--- ecommerce-mobile/app/product/[id].tsx
+++ ecommerce-mobile/app/product/[id].tsx
@@ -1,7 +1,15 @@
 import { Text } from "@/components/ui/text";
 import { useLocalSearchParams } from "expo-router";
+import products from "@/assets/products.json";
 
 export default function ProductDetailsScreen() {
-  const { id } = useLocalSearchParams();
-  return <Text>Product Details {id}</Text>;
+  const { id } = useLocalSearchParams<{ id: string }>();
+
+  const product = products.find((product) => product.id === Number(id));
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
+  return <Text>Product Details {product.name}</Text>;
 }
