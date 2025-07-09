ece192a367743025b23a41b4f91ad2f71ea07ed5
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-07-09 17:06:17 -0600

    feat: implement product details page and refactor product list item

--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,9 +1,6 @@
 import { Card } from "@/components/ui/card";
 import { Text } from "@/components/ui/text";
-import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
-import { Button, ButtonText } from "@/components/ui/button";
-import { Box } from "@/components/ui/box";
 import { Image } from "@/components/ui/image";
 import { Link } from "expo-router";
 import { Pressable } from "react-native";
@@ -24,25 +21,9 @@ export default function ProductListItem({ product }: { product: any }) {
           <Text className="text-sm font-normal mb-2 text-typography-700">
             {product.name}
           </Text>
-          <VStack className="mb-6">
-            <Heading size="md" className="mb-4">
-              {`$${product.price}`}
-            </Heading>
-            <Text size="sm">{product.description}</Text>
-          </VStack>
-          <Box className="flex-col sm:flex-row">
-            <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-              <ButtonText size="sm">Add to cart</ButtonText>
-            </Button>
-            <Button
-              variant="outline"
-              className="px-4 py-2 border-outline-300 sm:flex-1"
-            >
-              <ButtonText size="sm" className="text-typography-600">
-                Wishlist
-              </ButtonText>
-            </Button>
-          </Box>
+          <Heading size="md" className="mb-4">
+            {`$${product.price}`}
+          </Heading>
         </Card>
       </Pressable>
     </Link>
