70e89131caa6d2c01e7b4c726c4ef1db86664dd4
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-09-01 18:01:53 -0600

    chore(api): Remove .env.example and enhance server log message

--- api/src/index.ts
+++ api/src/index.ts
@@ -38,11 +38,12 @@
 import express from "express";
 
 const app = express();
+const port = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
