5996001a3cc316d94e53512991df29614a22f0fe
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-09-04 19:18:38 -0600

    feat(products): Implement API for product CRUD operations

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-// import { db } from '../../db/index.js';
-// import { productsTable } from '../../db/productsSchema.js';
-// import { eq } from 'drizzle-orm';
+import { db } from "../../db/index.js";
+import { productsTable } from "../../db/productsSchema.js";
+import { eq } from "drizzle-orm";
 // import _ from 'lodash';
 
 // export async function listProducts(req: Request, res: Response) {
@@ -84,17 +84,43 @@ import { Request, Response } from "express";
 //   }
 // }
 
-export function listProducts(req: Request, res: Response) {
-  res.send("listProducts");
+export async function listProducts(req: Request, res: Response) {
+  try {
+    const products = await db.select().from(productsTable);
+    res.json(products);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
-export function getProductById(req: Request, res: Response) {
-  res.send("getProductById");
+export async function getProductById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const [product] = await db
+      .select()
+      .from(productsTable)
+      .where(eq(productsTable.id, Number(id)));
+
+    if (!product) {
+      res.status(404).send({ message: "Product not found" });
+    } else {
+      res.json(product);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
-export function createProduct(req: Request, res: Response) {
-  console.log(req.body);
-  res.send("createProduct");
+export async function createProduct(req: Request, res: Response) {
+  try {
+    const [product] = await db
+      .insert(productsTable)
+      .values(req.body)
+      .returning();
+    res.status(201).json(product);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
 export function updateProduct(req: Request, res: Response) {
