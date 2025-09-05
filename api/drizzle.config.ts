d9d08117fbdf88da0ede4f7283dafbf2d602ad80
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-09-04 18:08:37 -0600

    feat(db): Generate and migrate database and Drizzel setup

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: [
-    './src/db/productsSchema.ts',
-    './src/db/usersSchema.ts',
-    './src/db/ordersSchema.ts',
-  ],
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: ["./src/db/productsSchema.ts"],
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
