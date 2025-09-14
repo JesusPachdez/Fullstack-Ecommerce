2bc29f2e98900001b8b631a1fe29aca3c7438de2
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-09-13 20:51:59 -0600

    feat: fix ts build

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,4 +1,4 @@
-// import { Router } from 'express';
+import { Router } from "express";
 import {
   listProducts,
   getProductById,
@@ -8,34 +8,11 @@ import {
 } from "./productsController.js";
 import { validateData } from "../../middlewares/validationMiddleware.js";
 
-import { Router } from "express";
 import {
   createProductSchema,
   updateProductSchema,
 } from "../../db/productsSchema.js";
 import { verifySeller, verifyToken } from "../../middlewares/authMiddleware.js";
-
-// const router = Router();
-
-// router.get('/', listProducts);
-// router.get('/:id', getProductById);
-// router.post(
-//   '/',
-//   verifyToken,
-//   verifySeller,
-//   validateData(createProductSchema),
-//   createProduct
-// );
-// router.put(
-//   '/:id',
-//   verifyToken,
-//   verifySeller,
-//   validateData(updateProductSchema),
-//   updateProduct
-// );
-// router.delete('/:id', verifyToken, verifySeller, deleteProduct);
-
-// export default router;
 
 const router = Router();
 
