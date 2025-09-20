a0a8ae6d4207d16f3360df2d28a0aa5e664995b8
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-09-19 23:46:25 -0600

    feat: Implement responsive product listing and detail screens

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,14 +1,17 @@
-import { FlatList } from "react-native";
+import { FlatList, useWindowDimensions } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
+import { Platform } from "react-native";
 
 export default function Home() {
+  const { width } = useWindowDimensions();
+  const numberOfColumns = width > 700 ? 3 : 2;
   return (
     <FlatList
+      key={numberOfColumns}
       data={products}
-      numColumns={2}
-      key={`flatlist-${2}`}
-      contentContainerClassName="gap-2"
+      numColumns={numberOfColumns}
+      contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
       columnWrapperClassName="gap-2"
       renderItem={({ item }) => <ProductListItem product={item} />}
     />
