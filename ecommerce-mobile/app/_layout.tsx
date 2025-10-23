50119f9ca6b0ba11e885945a48e1bbbb6a244124
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-10-22 18:01:50 -0600

    fix: replace tva with custom style function in Icon component & Resolves @gluestack-ui/nativewind-utils import error that was preventing app bundling

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -2,6 +2,8 @@ import "@/global.css";
 import { Stack } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Icon } from "@/components/ui/icon";
+import { ShoppingCart } from "lucide-react-native";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +11,11 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <GluestackUIProvider>
-        <Stack>
+        <Stack
+          screenOptions={{
+            headerRight: () => <Icon as={ShoppingCart} size="sm" />,
+          }}
+        >
           <Stack.Screen name="index" options={{ title: "Shop" }} />
           <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
         </Stack>
