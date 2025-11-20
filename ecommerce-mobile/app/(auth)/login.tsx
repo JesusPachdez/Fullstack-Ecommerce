0862d0b276406725d5166cb77d1c6edb13bec555
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-11-20 17:34:57 -0600

    feat(mobile): add authentication API and connect to login UI

--- ecommerce-mobile/app/(auth)/login.tsx
+++ ecommerce-mobile/app/(auth)/login.tsx
@@ -7,9 +7,33 @@ import { VStack } from "@/components/ui/vstack";
 import { EyeIcon, EyeOffIcon } from "@/components/ui/icon";
 import { HStack } from "@/components/ui/hstack";
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { login, signup } from "@/api/auth";
 
 export default function LoginScreen() {
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginMutation = useMutation({
+    mutationFn: () => login(email, password),
+    onSuccess: (data) => {
+      console.log("Logged in successfully", data);
+    },
+    onError: () => {
+      console.log("Failed to login");
+    },
+  });
+
+  const signupMutation = useMutation({
+    mutationFn: () => signup(email, password),
+    onSuccess: (data) => {
+      console.log("Signed up successfully", data);
+    },
+    onError: (error) => {
+      console.log("Failed to signup: ", error.message);
+    },
+  });
 
   const handleState = () => {
     setShowPassword((showState) => {
@@ -18,29 +42,40 @@ export default function LoginScreen() {
   };
 
   return (
-    <FormControl className="p-4 border rounded-lg max-w-[500px] border-outline-300 bg-white m-2">
+    <FormControl
+      isInvalid={loginMutation?.isError || signupMutation?.isError}
+      className="p-4 border rounded-lg max-w-[500px] border-outline-300 bg-white m-2"
+    >
       <VStack space="xl">
         <Heading className="text-typography-900 leading-3 pt-3">Login</Heading>
         <VStack space="xs">
           <Text className="text-typography-500">Email</Text>
           <Input className="min-w-[250px]">
-            <InputField type="text" />
+            <InputField type="text" value={email} onChangeText={setEmail} />
           </Input>
         </VStack>
         <VStack space="xs">
           <Text className="text-typography-500">Password</Text>
           <Input className="text-center">
-            <InputField type={showPassword ? "text" : "password"} />
+            <InputField
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChangeText={setPassword}
+            />
             <InputSlot className="pr-3" onPress={handleState}>
               <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
             </InputSlot>
           </Input>
         </VStack>
         <HStack space="sm">
-          <Button className="flex-1" variant="outline" onPress={() => {}}>
+          <Button
+            className="flex-1"
+            variant="outline"
+            onPress={() => signupMutation.mutate()}
+          >
             <ButtonText>Sign up</ButtonText>
           </Button>
-          <Button className="flex-1" onPress={() => {}}>
+          <Button className="flex-1" onPress={() => loginMutation.mutate()}>
             <ButtonText>Sign in</ButtonText>
           </Button>
         </HStack>
