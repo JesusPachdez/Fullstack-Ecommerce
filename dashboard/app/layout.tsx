4eceba11f5e1077044bddf7144ae7a18a5959358
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-12-04 18:03:16 -0600

    feat: setup Gluestack UI

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import '@/app/globals.css';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,8 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-      </body>
+        <GluestackUIProvider mode="dark">
+            {children}
+          </GluestackUIProvider>
+        </body>
     </html>
   );
 }
