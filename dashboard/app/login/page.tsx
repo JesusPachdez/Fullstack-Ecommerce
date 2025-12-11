4cd61e3980f34d4f27532999536e767f2867f930
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2025-12-10 22:57:25 -0600

    feat(ui): introduce Box component and refactor Login page for improved layout and consistency

--- dashboard/app/login/page.tsx
+++ dashboard/app/login/page.tsx
@@ -8,45 +8,54 @@ import { Text } from "@/components/ui/text";
 import { Input, InputField } from "@/components/ui/input";
 import { HStack } from "@/components/ui/hstack";
 import { Button, ButtonText } from "@/components/ui/button";
+import { Box } from "@/components/ui/box";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <FormControl
-      isInvalid={false}
-      className="p-4 border rounded-lg max-w-[500px] border-outline-300 bg-white m-2"
-    >
-      <VStack space="xl">
-        <Heading className="text-typography-900 leading-3 pt-3 color-black">
-          Login
-        </Heading>
-        <VStack space="xs">
-          <Text className="text-typography-500 color-black">Email</Text>
-          <Input className="min-w-[250px]">
-            <InputField type="text" value={email} onChangeText={setEmail} />
-          </Input>
+    <Box className="flex-1 min-h-screen justify-center items-center">
+      <FormControl
+        isInvalid={false}
+        className="p-4 border rounded-lg max-w-[500px] w-full border-outline-300 bg-white m-2"
+      >
+        <VStack space="xl">
+          <Heading className="text-typography-900 leading-3 pt-3 color-black">
+            Login
+          </Heading>
+          <VStack space="xs">
+            <Text className="text-typography-500 color-black">Email</Text>
+            <Input className="min-w-[250px]">
+              <InputField
+                className="color-black"
+                type="text"
+                value={email}
+                onChangeText={setEmail}
+              />
+            </Input>
+          </VStack>
+          <VStack space="xs">
+            <Text className="text-typography-500 color-black">Password</Text>
+            <Input className="text-center">
+              <InputField
+                className="color-black"
+                type="password"
+                value={password}
+                onChangeText={setPassword}
+              />
+            </Input>
+          </VStack>
+          <HStack space="sm">
+            <Button className="flex-1" variant="outline" onPress={() => {}}>
+              <ButtonText className="color-black">Sign up</ButtonText>
+            </Button>
+            <Button className="flex-1" onPress={() => {}}>
+              <ButtonText className="color-black">Sign in</ButtonText>
+            </Button>
+          </HStack>
         </VStack>
-        <VStack space="xs">
-          <Text className="text-typography-500 color-black">Password</Text>
-          <Input className="text-center">
-            <InputField
-              type="password"
-              value={password}
-              onChangeText={setPassword}
-            />
-          </Input>
-        </VStack>
-        <HStack space="sm">
-          <Button className="flex-1" variant="outline" onPress={() => {}}>
-            <ButtonText className="color-black">Sign up</ButtonText>
-          </Button>
-          <Button className="flex-1" onPress={() => {}}>
-            <ButtonText className="color-black">Sign in</ButtonText>
-          </Button>
-        </HStack>
-      </VStack>
-    </FormControl>
+      </FormControl>
+    </Box>
   );
 }
